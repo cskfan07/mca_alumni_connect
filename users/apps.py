07e508053a86d0a3915d42395dbf034bac658387
Ankit Gupta <91899413+cskfan07@users.noreply.github.com>
2026-01-04 13:56:50 +0530

    Clean up UsersConfig by removing unused code
    Removed unused imports and the ready method for MongoDB connection.

--- users/apps.py
+++ users/apps.py
@@ -1,20 +1,3 @@
-from django.apps import AppConfig
-import os
-from mongoengine import connect, connection
-
+# users/apps.py
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
-
-    def ready(self):
-        MONGO_URI = os.getenv("MONGO_URI")
-        MONGO_DB = os.getenv("MONGO_DB_NAME")
-
-        if MONGO_URI and MONGO_DB:
-            if 'default' not in connection._connections:
-                connect(
-                    db=MONGO_DB,
-                    host=MONGO_URI,
-                    alias='default',
-                
-                )
