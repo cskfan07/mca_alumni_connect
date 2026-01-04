cdd6363898107f21b180eb1dd650a160de1000f0
Ankit Gupta <91899413+cskfan07@users.noreply.github.com>
2026-01-04 19:05:15 +0530

    Fix app name quotes and add MongoDB initialization
    Updated name attribute to use double quotes and added ready method to initialize MongoDB.

--- users/apps.py
+++ users/apps.py
@@ -1,4 +1,9 @@
 
 from django.apps import AppConfig
+
 class UsersConfig(AppConfig):
-    name = 'users'
+    name = "users"
+
+    def ready(self):
+        from .mongo import init_mongo
+        init_mongo()
