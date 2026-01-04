d86607f47585ece3cdab660fb169c8e32fcb7a53
Ankit Gupta <91899413+cskfan07@users.noreply.github.com>
2026-01-04 12:09:41 +0530

    Implement MongoEngine connection in UsersConfig
    Add MongoEngine connection in ready method for multi-process support.

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
-
+import os
+from mongoengine import connect
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        """
+        MongoEngine connect after Django process fork.
+        Safe for Render / gunicorn multi-process.
+        """
+        MONGO_URI = os.getenv("MONGO_URI")
+        MONGO_DB = os.getenv("MONGO_DB_NAME")
+        if MONGO_URI and MONGO_DB:
+            connect(db=MONGO_DB, host=MONGO_URI)
